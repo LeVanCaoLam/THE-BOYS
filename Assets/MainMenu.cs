e49cf27d87f0cbf83aa28771cec0075e57c34284
DepTraiNhatSV <tungvntps41465@gmail.com>
2024-12-01 02:22:08 -0800

    tung
    mainmenu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Phương thức để bắt đầu trò chơi
+    public void PlayGame()
+    {
+        // Kiểm tra và tải cảnh "Game"
+        if (Application.CanStreamedLevelBeLoaded("Game"))
+        {
+            SceneManager.LoadScene("Game");
+        }
+        else
+        {
+            Debug.LogError("Cảnh 'Game' chưa được thêm vào Build Settings.");
+        }
+    }
+
+    // Phương thức để thoát trò chơi
+    public void QuitGame()
+    {
+        Debug.Log("Thoát trò chơi");
+        Application.Quit();
+    }
+}
