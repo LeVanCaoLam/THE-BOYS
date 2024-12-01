4ab1b70c9253320778fcaa121d94a98b4da5b96a
minhpd38783 <minhpdps38783@gmail.com>
2024-12-01 20:56:02 +0700

    Làm lại Menu, Thêm SoundTrack Menu

---
+++ Assets/Script/MainMenuGame.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuGame : MonoBehaviour
+{
+    public Button playButton;
+    public Button exitButton;
+    public AudioSource menuAudio;
+    public AudioClip menuClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuAudio = GetComponent<AudioSource>();
+        menuClip = GetComponent<AudioClip>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (playButton != null)
+        {
+            playButton = GetComponent<Button>();
+        }
+
+        if (exitButton != null)
+        {
+            exitButton = GetComponent<Button>();
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Map1");
+        Debug.Log("Vào game thành công");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Đã thoát game thành công");
+    }
+}
