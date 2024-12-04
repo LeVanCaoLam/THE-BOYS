93c308b404233a9c5e1877c0e4d72262fb8e0c26
LeVanCaoLam <levancaolam05@gmail.com>
2024-12-04 16:51:50 +0700

    light

---
+++ Assets/Cao Lâm/Scrip/LightScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    public float timeDayNight = 1f; // thời gian quy định
+    public Light light;
+
+    public Gradient gradient;
+
+    public float rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotationSpeed = 360f / (timeDayNight * 60f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        if (light != null && gradient != null)
+        {
+            float time = Mathf.PingPong(Time.time / (timeDayNight * 60f), 1f);
+            light.color = gradient.Evaluate(time);
+        }
+    }
+}
