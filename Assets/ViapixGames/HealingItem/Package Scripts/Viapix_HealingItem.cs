e8294c9761d8c9b5d65047a316e63ce939ecd0c8
minhpd38783 <minhpdps38783@gmail.com>
2024-12-08 22:33:56 +0700

    TextMeshPro Thành công

--- Assets/ViapixGames/HealingItem/Package Scripts/Viapix_HealingItem.cs
+++ Assets/ViapixGames/HealingItem/Package Scripts/Viapix_HealingItem.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Viapix_PlayerParams;
 
 namespace Viapix_HealingItem
 {
@@ -23,18 +22,6 @@ namespace Viapix_HealingItem
         void Update()
         {
             transform.Rotate(rotationSpeedX, rotationSpeedY, rotationSpeedZ);
-        }
-
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                playerObj.GetComponent<Viapix_PlayerHP>().playerHP += healingAmount;
-
-                Destroy(gameObject);
-
-                print("Player HP: " + playerObj.GetComponent<Viapix_PlayerHP>().playerHP);
-            }
         }
     }
 }
