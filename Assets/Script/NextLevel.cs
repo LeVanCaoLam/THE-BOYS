abd35107158782eaa91854ac64e9267c005b2c5a
minhpd38783 <minhpdps38783@gmail.com>
2024-12-09 16:33:20 +0700

    chuyển level thành công sang scene mới, fix lỗi GameSession bị mất

---
+++ Assets/Script/NextLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour
+{
+    [SerializeField] private int nextSceneIndex;
+    [SerializeField] private float transitionDelay = 1f; // Thời gian delay trước khi chuyển scene
+    [SerializeField] AudioSource portalSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            portalSound.PlayOneShot(portalSound.clip);
+            StartCoroutine(LoadNextSceneWithDelay());
+        }
+    }
+
+    private IEnumerator LoadNextSceneWithDelay()
+    {
+        // Có thể thêm hiệu ứng fade out ở đây nếu muốn
+        yield return new WaitForSeconds(transitionDelay);
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
